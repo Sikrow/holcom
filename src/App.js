b0451eb9f382a4a43b5f1ba558d28f476cbe8d15
Michael <sikrow96@gmail.com>
2021-11-28 21:39:32 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, {useState, useEffect} from 'react'
+
+import { commerce } from './lib/commerce'; //this does all the backend stuff
+
+import { Products, Navbar } from './components'
+// the above is the same as below, but you need to have an index.js file in the components folder. 
+//import Products  from './components/Products/Products';
+//import Navbar from './components/Navbar/Navbar';
+
+const App = () => {
+
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState({})
+
+    const fetchProducts = async () => {
+        const { data } = await commerce.products.list();
+
+        setProducts(data);
+
+    }
+
+    const handleAddToCart = async (productId, quantity) => {
+        const item = await commerce.cart.add(productId, quantity);
+
+        setCart(item.cart);
+    }
+
+    const fetchCart = async () => {
+        const cart = await commerce.cart.retrieve();
+
+        setCart(cart)
+    }
+
+    useEffect(() => {
+        fetchProducts();
+        fetchCart();
+    }, []);
+
+    console.log(products);
+    console.log(cart)
+
+    return (
+
+        <div>
+            <Navbar/>
+            <Products products={products} onAddToCart={handleAddToCart}/>
+        </div>
+        
+    )
+}
+
+export default App;
+
