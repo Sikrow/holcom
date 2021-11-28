9323ec9fab26600e76003d5fbbe51a59bf72dfe4
Michael <sikrow96@gmail.com>
2021-11-28 21:53:55 +0100

    Add to cart

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ const App = () => {
     return (
 
         <div>
-            <Navbar/>
+            <Navbar totalItems={cart.total_items}/>
             <Products products={products} onAddToCart={handleAddToCart}/>
         </div>
         
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import logo from '../../assets/Fire_giant.png'
 import useStyles from './styles';
 
 
-const Navbar = () => {
+const Navbar = ({ totalItems }) => {
     const classes = useStyles();
     return (
         <>
@@ -20,7 +20,7 @@ const Navbar = () => {
                     <div className={classes.grow}/> 
                     <div className={classes.button}>
                         <IconButton aria-label="Kurv" color="inherit">
-                            <Badge badgeContent={2} color="secondary" >
+                            <Badge badgeContent={totalItems} color="secondary" >
                                 <ShoppingCart/>
                             </Badge>
                         </IconButton>
