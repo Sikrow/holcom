5190644b0a0d02c4e33b13e542fd158448e4c440
Michael Commerou <ace_mage@hotmail.com>
2021-11-29 14:12:03 +0100

    Review and payment start

--- src/components/CheckoutForm/Review.jsx
+++ src/components/CheckoutForm/Review.jsx
@@ -1,1 +1,15 @@
-//test
+import React from 'react'
+import { Typography, List, ListItem, ListItemText } from '@material-ui/core'
+
+const Review = () => {
+    return (
+        <>
+        <Typography variant="h6" gutterBottom> Ordre liste</Typography>
+        <List disablePadding>
+            
+        </List>
+        </>
+    )
+}
+
+export default Review
